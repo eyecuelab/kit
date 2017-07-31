3f9cbab09c844b13fb9463ce8c3a8449f3c78f6b
Efron Licht <efron.licht@eyecuelab.com>
2017-07-31 14:44:35 -0700

    restructuring of csync

--- csync/csync.go
+++ csync/csync.go
@@ -1,6 +1,9 @@
 package csync
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 var (
 	concurrentRequests Queue
@@ -12,47 +15,21 @@ const (
 )
 
 type Queue chan Signal
-type Signal struct{}
 
-type SignalFunc func() bool
+func (q *Queue) Full() bool {
+	return cap(*q) == len(*q)
+}
+
+type Signal struct{}
 
 func init() {
 	concurrentRequests = make(Queue, maxRequests)
 }
 
-//StartTimeoutRequest waits for a slot to open up in concurrentRequests within timeout seconds. Then, it sends a signal that it is one of the concurrent requests.
-//startRequest returns a function that frees up a slot in concurrentRequests. use startRequest by deferring it's return value;
-//i.e, defer StartRequest()
-//ex:
-// given a func doWork that we want to launch in a goroutine, we start doWork as follows:
-// go doWork() {defer StartRequest() //function logic here}
-func StartTimeoutRequest(timeout time.Duration) SignalFunc {
-	if WaitForConcurrentRequests(concurrentRequests, timeout) {
-		concurrentRequests <- Start
-		return func() bool {
-			<-concurrentRequests
-			return true
-		}
-	}
-	return FailedRequest
-}
-
-func FailedRequest() bool {
-	return false
-}
-
-func StartForeverRequest() func() {
-	concurrentRequests <- Start
-	return func() {
-		<-concurrentRequests
-	}
-}
-
 //WaitForConcurrentRequests waits for a slot to open up in the default concurentRequests channel within timeout duration.
 //It returns true if a slot opens up in time and false otherwise.
 func WaitForConcurrentRequests(requests Queue, timeout time.Duration) bool {
-	var t time.Duration
-	for ; len(requests) == cap(requests); t += 100 * time.Millisecond {
+	for t := time.Duration(0); requests.Full(); t += 100 * time.Millisecond {
 		time.Sleep(100 * time.Millisecond)
 		if t > timeout {
 			return false
@@ -62,19 +39,25 @@ func WaitForConcurrentRequests(requests Queue, timeout time.Duration) bool {
 }
 
 func WaitForeverForConcurrentRequests(requests Queue) {
-	for len(requests) == cap(requests) {
+	for requests.Full() {
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
-func NewChannelWithTimeout(max int, timeout time.Duration) (maxRequests Queue, startRequest SignalFunc) {
-	concurrentRequests := make(Queue, max)
-	if WaitForConcurrentRequests(concurrentRequests, timeout) {
-		concurrentRequests <- Start
-		return concurrentRequests, func() bool {
-			<-concurrentRequests
-			return true
+//NO_OP does nothing.
+func NO_OP() {}
+
+func NewChannelWithTimeout(max int, timeout time.Duration, errors chan error) (concurrentRequests Queue, startRequest func(caller string) func()) {
+	concurrentRequests = make(Queue, max)
+	startRequest = func(caller string) func() {
+		if WaitForConcurrentRequests(concurrentRequests, timeout) {
+			concurrentRequests <- Start
+			return func() {
+				<-concurrentRequests
+			}
 		}
+		errors <- fmt.Errorf("%s: NewChannelWithTimeout: timeout after %f seconds", caller, timeout.Seconds())
+		return NO_OP
 	}
-	return concurrentRequests, FailedRequest
+	return concurrentRequests, startRequest
 }
