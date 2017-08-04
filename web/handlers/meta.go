353988a84bf8f1ef37fa03a5f6126f793f3c2ce0
Paul Walker <paul.walker@eyecuelab.com>
2017-08-03 18:44:29 -0700

    generic handlers

---
+++ web/handlers/meta.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/google/jsonapi"
+	"github.com/labstack/echo"
+	"github.com/spf13/viper"
+)
+
+func Healthz(c echo.Context) error {
+	if c.QueryParam("oops") == "1" {
+		panic("oops")
+	}
+	if c.QueryParam("apierr") == "1" {
+		return &jsonapi.ErrorObject{
+			Title:  "Api Error",
+			Status: fmt.Sprintf("%d", http.StatusBadRequest),
+			Detail: "missing parameters: foo,bar",
+		}
+	}
+	if c.QueryParam("500") == "1" {
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+	if c.QueryParam("400") == "1" {
+		return echo.NewHTTPError(http.StatusBadRequest, "Missing value.")
+	}
+	return c.String(http.StatusOK, "live")
+}
+
+func Config(c echo.Context) error {
+	return c.JSON(http.StatusOK, viper.AllSettings())
+}
