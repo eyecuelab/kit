9b112425ec43cd8c8e6d306eef9854460d7096c3
Paul Walker <paul.walker@eyecuelab.com>
2017-08-15 14:39:33 -0700

    support env var for database scheme, register dialect if it does not exist in gorm

--- db/psql/psql.go
+++ db/psql/psql.go
@@ -3,6 +3,8 @@ package psql
 import (
 	"errors"
 
+	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
+	"github.com/eyecuelab/kit/log"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/spf13/cobra"
@@ -11,19 +13,28 @@ import (
 
 var (
 	Db         *gorm.DB
+	DbUrl      string
+	Scheme     string
 	Error      error
 	references []*gorm.DB
 )
 
 func init() {
 	cobra.OnInitialize(connect)
+	viper.SetDefault("database_scheme", "postgres")
 }
 
 func connect() {
-	dbUrl := viper.GetString("database_url")
-	if dbUrl == "" {
+	Scheme = viper.GetString("database_scheme")
+	DbUrl = viper.GetString("database_url")
+
+	if DbUrl == "" {
 		Error = errors.New("Missing database url")
 	} else {
-		Db, Error = gorm.Open("postgres", dbUrl)
+		if Scheme != "postgres" {
+			log.Infof("Registering dialect: %s", Scheme)
+			gorm.RegisterDialect(Scheme, gorm.DialectsMap["postgres"])
+		}
+		Db, Error = gorm.Open(Scheme, DbUrl)
 	}
 }
