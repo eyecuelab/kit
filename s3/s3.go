d1315dbb4bbda00f0d744a4b06a97c55a23bf161
Eric Hanken <eric.hanken@eyecuelab.com>
2017-08-22 19:13:28 -0700

    use s3 bucket from env

--- s3/s3.go
+++ s3/s3.go
@@ -36,7 +36,7 @@ func Upload(b []byte, key string) (*s3manager.UploadOutput, error) {
 	fileType := http.DetectContentType(b)
 
 	params := &s3manager.UploadInput{
-		Bucket:      aws.String("lub-assets-test"),
+		Bucket:      aws.String(viper.GetString("aws_bucket")),
 		Key:         aws.String(key),
 		Body:        fileBytes,
 		ContentType: aws.String(fileType),
