c0943f08d02ea16aa210399ee34518dabeda89af
Efron Licht <efron.licht@eyecuelab.com>
2017-11-16 12:51:42 -0800

    more testing for error_handler.go"

--- web/error_handler.go
+++ web/error_handler.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -14,26 +15,51 @@ import (
 	"github.com/lib/pq"
 )
 
+//these are for internal testing.
+type errorHandlerCode byte
+
+const (
+	alreadyCommited errorHandlerCode = iota
+	methodIsHead
+	noContent
+	rendered
+	statusOver500
+	handledError
+	nilErr
+)
+
 var pq500s = map[string]bool{
 	"undefined_function": true,
 }
 
-func ErrorHandler(err error, c echo.Context) {
+func ErrorHandler(err error, c echo.Context) errorHandlerCode {
 	if c.Response().Committed {
-		return
+		return alreadyCommited
+	}
+	if err == nil {
+		logErr(errors.New("nil error sent into ErrorHandler"), c)
+		return nilErr
 	}
 
 	status, apiError := toApiError(err)
+
 	if c.Request().Method == "HEAD" {
 		if err := c.NoContent(status); err != nil {
 			logErr(err, c)
+			return noContent
 		}
-	} else if renderedErr := renderApiErrors(c, apiError); err != nil {
+		return methodIsHead
+	}
+	if renderedErr := renderApiErrors(c, apiError); renderedErr != nil {
 		logErr(renderedErr, c) //are we supposed to log this error twice?
 		logErr(err, c)
-	} else if status >= 500 {
-		logErr(err, c)
+		return rendered
 	}
+	if status >= 500 {
+		logErr(err, c)
+		return statusOver500
+	}
+	return handledError
 }
 
 func logErr(err error, c echo.Context) {
@@ -42,15 +68,21 @@ func logErr(err error, c echo.Context) {
 }
 
 func toApiError(err error) (status int, apiErr *jsonapi.ErrorObject) {
-
 	var (
 		detail string
 		code   string
 	)
 	status = http.StatusInternalServerError
 	switch err := err.(type) {
+	case nil:
+		panic("nil error sent into toApiError")
+
 	case *jsonapi.ErrorObject:
-		status, _ = strconv.Atoi(err.Status)
+		if status, convErr := strconv.Atoi(err.Status); convErr == nil {
+			return status, err
+		}
+		err.Detail += fmt.Sprintf(" bad status: %s", err.Status)
+
 		return status, err
 
 	case *echo.HTTPError:
@@ -58,20 +90,24 @@ func toApiError(err error) (status int, apiErr *jsonapi.ErrorObject) {
 		if err.Message != nil {
 			detail = fmt.Sprint(err.Message)
 		}
+		return status, valuesToApiError(status, http.StatusText(status), detail, code)
+
 	case *pq.Error:
 		detail = err.Message
 		code = err.Code.Name()
 		if _, ok := pq500s[code]; !ok {
 			status = http.StatusBadRequest
 		}
+		return status, valuesToApiError(status, http.StatusText(status), detail, code)
+
 	case govalidator.Errors:
 		status, detail = http.StatusBadRequest, err.Error()
+		return status, valuesToApiError(status, http.StatusText(status), detail, code)
+
 	default:
 		detail = err.Error()
-
+		return status, valuesToApiError(status, http.StatusText(status), detail, code)
 	}
-
-	return status, valuesToApiError(status, http.StatusText(status), detail, code)
 }
 
 func renderApiErrors(c echo.Context, errors ...*jsonapi.ErrorObject) (err error) {
@@ -84,7 +120,6 @@ func renderApiErrors(c echo.Context, errors ...*jsonapi.ErrorObject) (err error)
 		return err
 	}
 	return c.Blob(code, jsonapi.MediaType, b.Bytes())
-
 }
 
 func valuesToApiError(status int, title, detail, code string) *jsonapi.ErrorObject {
--- web/error_handler_test.go
+++ web/error_handler_test.go
@@ -1,21 +1,118 @@
 package web
 
 import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/google/jsonapi"
+	"github.com/lib/pq"
+
+	"github.com/eyecuelab/kit/errorlib"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestErrorHandler(t *testing.T) {
+const (
+	someErr errorlib.ErrorString = "some error"
+)
 
+type noContentContext struct {
+	echo.Context
+}
+
+func (ctx noContentContext) NoContent(code int) error {
+	return someErr
+}
+
+var _ echo.Context = noContentContext{}
+
+func newContext(method string) echo.Context {
+	req := httptest.NewRequest(method, "http://localhost.com/whatever", new(bytes.Buffer))
+	rec := httptest.NewRecorder()
+	return echo.New().NewContext(req, rec)
+}
+
+func TestErrorHandler(t *testing.T) {
+	ctx := newContext("GET")
+	ctx.Response().Committed = true
+	assert.Equal(t, alreadyCommited, ErrorHandler(someErr, ctx))
+
+	noContentCtx := noContentContext{newContext("HEAD")}
+	assert.Equal(t, noContent, ErrorHandler(someErr, noContentCtx))
+
+	ctx = newContext("HEAD")
+	var err error = &echo.HTTPError{Code: 1222, Message: "magic error"}
+	assert.Equal(t, methodIsHead, ErrorHandler(err, ctx))
+
+	ctx = newContext("GET")
+	assert.Equal(t, nilErr, ErrorHandler(nil, ctx))
+
+	ctx = newContext("GET")
+	//httpErr := echo.HTTPError{Code: 300, Message: "magic error", Inner: someErr}
+	//assert.Equal(t, rendered, ErrorHandler(&httpErr, ctx))
 }
 
 func Test_logErr(t *testing.T) {
-
 }
 
 func Test_toApiError(t *testing.T) {
+	t.Run("panic!", func(t *testing.T) {
+		defer func() {
+			recover()
+		}()
+		toApiError(nil)
+		t.Error("should have panicked")
+	})
+
+	errObj := jsonapi.ErrorObject{Status: "200"}
+	status, apiErr := toApiError(&errObj)
+	assert.Equal(t, 200, status)
+	assert.Equal(t, errObj, *apiErr)
+
+	//bad status
+	errObj = jsonapi.ErrorObject{Status: "foobar"}
+	wantApiErr := jsonapi.ErrorObject{Status: "foobar", Detail: " bad status: foobar"}
+	gotStatus, gotApiErr := toApiError(&errObj)
+	assert.Equal(t, http.StatusInternalServerError, gotStatus)
+	assert.Equal(t, wantApiErr, *apiErr)
+
+	//httperr
+	wantApiErr = jsonapi.ErrorObject{
+		Status: "404",
+		Title:  http.StatusText(404),
+		Detail: "not found",
+	}
+
+	wantStatus := 404
+	httpErr := echo.HTTPError{Code: 404, Message: "not found", Inner: someErr}
+	gotStatus, gotApiErr = toApiError(&httpErr)
+	assert.Equal(t, wantStatus, gotStatus)
+	assert.Equal(t, wantApiErr, *gotApiErr)
+	//pqErr
+	const (
+		msg      = "ok"
+		code     = "0100C"
+		codeName = "dynamic_result_sets_returned"
+	)
+	pqErr := pq.Error{Message: msg, Code: "0100C"}
+	wantStatus = http.StatusBadRequest
+	wantApiErr = jsonapi.ErrorObject{
+		Status: toStr(http.StatusBadRequest),
+		Detail: msg,
+		Code:   codeName,
+		Title:  "Bad Request"}
+	gotStatus, gotApiErr = toApiError(&pqErr)
+	assert.Equal(t, wantStatus, gotStatus)
+	assert.Equal(t, wantApiErr, *gotApiErr)
 
 }
 
+func toStr(n int) string {
+	return fmt.Sprintf("%d", n)
+}
 func Test_renderApiErrors(t *testing.T) {
 
 }
