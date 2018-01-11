a5c385749405a29fb8bb4b0c33cada07e1c84a94
Efron Licht <efron.licht@eyecuelab.com>
2018-01-11 12:57:48 -0800

    restored and fixed tests

--- umath/functional_test.go
+++ umath/functional_test.go
@@ -1,37 +1,37 @@
 package umath
 
-// import (
-// 	"testing"
-//
-// 	"github.com/stretchr/testify/assert"
-// )
-//
-// func isEven(n uint) bool { return n%2 == 0 }
-// func TestFilter(t *testing.T) {
-// 	a := []uint{2, 8, 7}
-// 	assert.Equal(t, []uint{2, 8}, Filter(isEven, a))
-//
-// }
-//
-// func leftshift(n uint) uint { return n << 1 }
-//
-// func TestMap(t *testing.T) {
-// 	a := Range(0, 5, 1)
-// 	want := Range(0, 10, 2)
-// 	assert.Equal(t, want, Map(leftshift, a))
-// }
-//
-// func add(a, b uint) uint { return a + b }
-// func TestReduce(t *testing.T) {
-// 	a := Range(0, 5, 1)
-// 	assert.Equal(t, Sum(a...), Reduce(add, a[0], a[1:]...))
-// }
-//
-// func TestAccumulate(t *testing.T) {
-//
-// 	a := Range(0, 5, 1)
-// 	want := []uint{0, 1, 3, 6, 10}
-// 	assert.Equal(t, want, Accumulate(add, a))
-//
-// 	assert.Empty(t, Accumulate(add, nil))
-// }
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func isEven(n uint) bool { return n%2 == 0 }
+func TestFilter(t *testing.T) {
+	a := []uint{2, 8, 7}
+	assert.Equal(t, []uint{2, 8}, Filter(isEven, a))
+
+}
+
+func leftshift(n uint) uint { return n << 1 }
+
+func TestMap(t *testing.T) {
+	a := Range(0, 5, 1)
+	want := Range(0, 10, 2)
+	assert.Equal(t, want, Map(leftshift, a))
+}
+
+func add(a, b uint) uint { return a + b }
+func TestReduce(t *testing.T) {
+	a := Range(0, 5, 1)
+	assert.Equal(t, Sum(a...), Reduce(add, a[0], a[1:]...))
+}
+
+func TestAccumulate(t *testing.T) {
+
+	a := Range(0, 5, 1)
+	want := []uint{0, 1, 3, 6, 10}
+	assert.Equal(t, want, Accumulate(add, a))
+
+	assert.Empty(t, Accumulate(add, nil))
+}
