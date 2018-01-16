7c31e08d0a09b4e51fdb8735fb99832552e37f79
Efron Licht <efron.licht@eyecuelab.com>
2018-01-16 10:55:41 -0800

    fixed null pointer exception for Get() and Dir()

--- assets/assets.go
+++ assets/assets.go
@@ -1,6 +1,8 @@
 package assets
 
-import "errors"
+import (
+	"github.com/eyecuelab/kit/errorlib"
+)
 
 type (
 	AssetGet     func(string) ([]byte, error)
@@ -11,12 +13,20 @@ type (
 	}
 )
 
+const (
+	ErrManagerNotSet errorlib.ErrorString = "asset manager is not set"
+	ErrNoGetFunc     errorlib.ErrorString = "asset manager has no get func"
+	ErrNoDirFunc     errorlib.ErrorString = "asset manager has no dir func"
+)
+
 var Manager *AssetManager
 
 // Get retrieve static asset from client project data directory. This allows code in kit to use client project data dir
 func Get(name string) ([]byte, error) {
 	if Manager == nil {
-		return nil, errors.New("Manager is not set")
+		return nil, ErrManagerNotSet
+	} else if Manager.Get == nil {
+		return nil, ErrNoGetFunc
 	}
 
 	return Manager.Get(name)
@@ -24,7 +34,9 @@ func Get(name string) ([]byte, error) {
 
 func Dir(name string) ([]string, error) {
 	if Manager == nil {
-		return nil, errors.New("Manager is not set")
+		return nil, ErrManagerNotSet
+	} else if Manager.Dir == nil {
+		return nil, ErrNoDirFunc
 	}
 
 	return Manager.Dir(name)
