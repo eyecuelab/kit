4a446fadc36456669e6cb0153912f31329505930
Sasha Shamne <alexander.shamne@gmail.com>
2018-02-01 10:57:16 -0800

    Add linkedin oauth user data retriever by the access_token

---
+++ oauth/linkedin/linkedin.go
@@ -0,0 +1,44 @@
+package linkedin
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/parnurzeal/gorequest"
+)
+
+const (
+	// DataEndpoint linkedin profile data endpoint url
+	DataEndpoint = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address)"
+)
+
+// User linkedin user data
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"emailAddress"`
+	Headline  string
+	ErrorCode int `json:"errorCode"`
+	Message   string
+	Status    int
+}
+
+// UserInfo linkedin user info by the access token
+func UserInfo(accessToken string) (User, error) {
+	var user User
+
+	request := gorequest.New().Get(DataEndpoint)
+
+	_, body, errs := request.Param("format", "json").Param("oauth2_access_token", accessToken).End()
+
+	if errs != nil {
+		return user, errs[0]
+	}
+
+	err := json.Unmarshal([]byte(body), &user)
+	if user.Message != "" {
+		return user, errors.New(user.Message)
+	}
+	return user, err
+}
