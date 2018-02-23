9164b6efa75de674aa545fcfd248185a5ebd39aa
Sasha Shamne <alexander.shamne@gmail.com>
2018-02-22 16:53:10 -0800

    Refactor web integration testing package

---
+++ web/testing/integration/assert.go
@@ -0,0 +1,95 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/parnurzeal/gorequest"
+	"github.com/stretchr/testify/assert"
+)
+
+// AssertGetOK assert GET request is OK
+func AssertGetOK(t *testing.T, path string, token string) (resp gorequest.Response, body string, errs []error) {
+	resp, body, errs = Request("GET", path, token).End()
+	assert.Empty(t, errs)
+	AssertStatusOK(t, resp)
+
+	return resp, body, errs
+}
+
+// AssertAttr ...
+func AssertAttr(t *testing.T, i interface{}, m map[string]interface{}) {
+	if i == nil {
+		assert.True(t, false, "Expected Attributes got nil")
+		return
+	}
+	attrs := i.(map[string]interface{})
+	for k, v := range m {
+		assert.Equal(t, v, attrs[k])
+	}
+}
+
+// AssertLink ...
+func AssertLink(t *testing.T, i interface{}, names ...string) {
+	if i == nil {
+		assert.True(t, false, "Expected links got nil")
+		return
+	}
+	links := i.(map[string]interface{})
+	var link string
+	for _, name := range names {
+		link = links[name].(string)
+		assert.True(t, len([]rune(link)) > 0)
+	}
+}
+
+// AssertAction ...
+func AssertAction(t *testing.T, meta JSONAPIRespMeta, names ...string) {
+	var includes bool
+	for _, name := range names {
+		includes = false
+		for _, action := range meta.Actions {
+			if action.Name == name {
+				includes = true
+			}
+		}
+		assert.True(t, includes)
+	}
+}
+
+// AssertNoAction ...
+func AssertNoAction(t *testing.T, meta JSONAPIRespMeta, names ...string) {
+	var includes bool
+	for _, name := range names {
+		includes = false
+		for _, action := range meta.Actions {
+			if action.Name == name {
+				includes = true
+			}
+		}
+		assert.True(t, !includes)
+	}
+}
+
+// AssertStatusOK assert response status code is OK
+func AssertStatusOK(t *testing.T, resp gorequest.Response) {
+	ok := resp.StatusCode == 200 || resp.StatusCode == 201
+	if !ok {
+		t.Errorf("Expected status 200/201 but is %d\n%+v\n", resp.StatusCode, resp.Body)
+	}
+}
+
+// AssertStatusUnauthorized assert response status code is 401
+func AssertStatusUnauthorized(t *testing.T, resp gorequest.Response) {
+	ok := resp.StatusCode == 401
+	if !ok {
+		t.Errorf("Expected status 401 but is %d", resp.StatusCode)
+	}
+}
+
+// AssertStatusNotFound assert response status code is 404
+func AssertStatusNotFound(t *testing.T, resp gorequest.Response) {
+	ok := resp.StatusCode == 404
+	if !ok {
+		t.Errorf("Expected status 404 but is %d", resp.StatusCode)
+	}
+}
--- web/testing/integration/testing.go
+++ web/testing/integration/testing.go
@@ -8,7 +8,6 @@ import (
 	"github.com/eyecuelab/kit/web/meta"
 	"github.com/google/jsonapi"
 	"github.com/parnurzeal/gorequest"
-	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -22,15 +21,18 @@ type (
 	}
 	// JSONAPIRespData ...
 	JSONAPIRespData struct {
-		ID         string
-		Attributes interface{}     `json:"attributes"`
-		Meta       JSONAPIRespMeta `json:"meta"`
-		Links      interface{}     `json:"links"`
+		ID            string
+		Type          string
+		Attributes    interface{}     `json:"attributes"`
+		Meta          JSONAPIRespMeta `json:"meta"`
+		Links         interface{}     `json:"links"`
+		Relationships interface{}
 	}
 	// JSONAPIOneResp ...
 	JSONAPIOneResp struct {
-		Data   JSONAPIRespData        `json:"data"`
-		Errors []*jsonapi.ErrorObject `json:"errors"`
+		Data     JSONAPIRespData        `json:"data"`
+		Errors   []*jsonapi.ErrorObject `json:"errors"`
+		Included []JSONAPIRespData
 	}
 	// JSONAPIManyResp ...
 	JSONAPIManyResp struct {
@@ -88,63 +90,6 @@ func GetList(t *testing.T, path string, token string) (gorequest.Response, *JSON
 	data := unmarshalMany(t, body)
 
 	return resp, data, errs
-}
-
-// AssertGetOK assert GET request is OK
-func AssertGetOK(t *testing.T, path string, token string) (resp gorequest.Response, body string, errs []error) {
-	resp, body, errs = Request("GET", path, token).End()
-	assert.Empty(t, errs)
-	AssertStatusOK(t, resp)
-
-	return resp, body, errs
-}
-
-// AssertAttr ...
-func AssertAttr(t *testing.T, i interface{}, m map[string]interface{}) {
-	attrs := i.(map[string]interface{})
-	for k, v := range m {
-		assert.Equal(t, v, attrs[k])
-	}
-}
-
-// AssertLink ...
-func AssertLink(t *testing.T, i interface{}, names ...string) {
-	links := i.(map[string]interface{})
-	var link string
-	for _, name := range names {
-		link = links[name].(string)
-		assert.True(t, len([]rune(link)) > 0)
-	}
-}
-
-// AssertAction ...
-func AssertAction(t *testing.T, meta JSONAPIRespMeta, names ...string) {
-	var includes bool
-	for _, name := range names {
-		includes = false
-		for _, action := range meta.Actions {
-			if action.Name == name {
-				includes = true
-			}
-		}
-		assert.True(t, includes)
-	}
-}
-
-// AssertStatusOK assert response status code is OK
-func AssertStatusOK(t *testing.T, resp gorequest.Response) {
-	ok := resp.StatusCode == 200 || resp.StatusCode == 201
-	if !ok {
-		t.Errorf("Expected status 200/201 but is %d", resp.StatusCode)
-	}
-}
-
-// AssertStatusUnauthorized assert response status code is 401
-func AssertStatusUnauthorized(t *testing.T, resp gorequest.Response) {
-	ok := resp.StatusCode == 401
-	if !ok {
-		t.Errorf("Expected status 401 but is %d", resp.StatusCode)
-	}
 }
 
 func jsonAPIPayload(t *testing.T, attrs map[string]interface{}) (payload string) {
