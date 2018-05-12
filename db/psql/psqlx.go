0139f77c83575c4c9cb82714248a093eb64cd702
Eric Hanken <eric.hanken@eyecuelab.com>
2018-05-12 10:24:56 -0700

    Add Sqlx helpers

--- db/psql/psqlx.go
+++ db/psql/psqlx.go
@@ -2,22 +2,36 @@ package psql
 
 import (
 	"errors"
+	"fmt"
+	"github.com/jmoiron/sqlx"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/jmoiron/sqlx"
 
 	//register postgres dialect
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
 	_ "github.com/lib/pq"
 )
 
+const (
+	selectAll  = "select * from %s"
+	selectFind = "select * from %s where id = ?"
+)
+
 var (
-	DBx      *sqlx.DB
+	DBx      *IQSqlx
 	DBxError error
 )
 
 func init() {
 	cobra.OnInitialize(ConnectDBx)
+}
+
+type IQSqlx struct {
+	X *sqlx.DB
+}
+
+type IQModel interface {
+	TableName() string
 }
 
 func ConnectDBx() {
@@ -29,6 +43,17 @@ func ConnectDBx() {
 		DBxError = errors.New("Missing database_url")
 		return
 	}
-	DBx, DBxError = sqlx.Connect(scheme, url)
-	// DB.LogMode(true)
+	var dbx *sqlx.DB
+	dbx, DBxError = sqlx.Connect(scheme, url)
+	DBx = &IQSqlx{dbx}
+}
+
+func (iqx IQSqlx) All(m interface{}, ref IQModel) error {
+	clause := fmt.Sprintf(selectAll, ref.TableName())
+	return iqx.X.Select(m, clause)
+}
+
+func (iqx IQSqlx) Find(m IQModel, id string) error {
+	clause := fmt.Sprintf(selectFind, m.TableName())
+	return iqx.X.Get(m, clause, id)
 }
